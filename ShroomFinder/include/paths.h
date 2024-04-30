00a0c11189c8f6dd189804b08f138fbd1b2bfabd
nanoschi <78222596+Nanoschi@users.noreply.github.com>
2024-04-30 19:27:14 +0200

    added large map tiles

--- ShroomFinder/include/paths.h
+++ ShroomFinder/include/paths.h
@@ -2,10 +2,8 @@
 #include <string>
 
 namespace paths {
-	const std::string MapPath = "resources/germany.png";
-
 	const std::string TextureVertexShader = "resources/shaders/texture_shader/texture.vert";
 	const std::string TextureFragmentShader = "resources/shaders/texture_shader/texture.frag";
 
-	const std::string LargeTileDir = "C:/Users/janos/Desktop/QGIS/sections_large_compressed";
+	const std::string LargeTileDir = "resources/map_tiles_large";
 }
